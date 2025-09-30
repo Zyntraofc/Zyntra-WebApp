4ae960fec6cbe14afd80becfbf24d0f2bfd08835
ErickNeves07 <barbosa.erick.neves@gmail.com>
2025-09-30 07:19:43 -0300

    fix: Correção do redirecionamento após o login

--- src/main/java/org/example/servlet/ServletLogin.java
+++ src/main/java/org/example/servlet/ServletLogin.java
@@ -31,7 +31,7 @@ public class ServletLogin extends HttpServlet {
                 HttpSession session = req.getSession(true);
                 session.setAttribute("usuarioLogado", listagem.getEmail());
 
-                req.getRequestDispatcher("/view/CrudStatusAprovacao.jsp").forward(req, resp);
+                req.getRequestDispatcher("/ServletListarStatusAprovacao").forward(req, resp);
             } else {
                 req.setAttribute("erroLogin", "Usuário ou senha incorretos!");
                 req.setAttribute("emailDigitado", email);
