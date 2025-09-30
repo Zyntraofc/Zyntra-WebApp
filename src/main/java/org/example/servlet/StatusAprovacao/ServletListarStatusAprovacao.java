3c514a4d4ee66208e486fda564fad875b7286d09
ErickNeves07 <barbosa.erick.neves@gmail.com>
2025-09-30 07:31:37 -0300

    feat: Servlet para listagem dos status de aprovação

--- src/main/java/org/example/servlet/StatusAprovacao/ServletListarStatusAprovacao.java
+++ src/main/java/org/example/servlet/StatusAprovacao/ServletListarStatusAprovacao.java
@@ -1,4 +1,20 @@
 package org.example.servlet.StatusAprovacao;
 
-public class ServletListarStatusAprovacao {
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.example.dao.StatusAprovacaoDAO;
+import org.example.model.StatusAprovacao;
+import java.util.List;
+
+@WebServlet("/ListarStatusAprovacao")
+public class ServletListarStatusAprovacao extends HttpServlet{
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+        StatusAprovacaoDAO statusdao = new StatusAprovacaoDAO();
+        List<StatusAprovacao> status = statusdao.listarTodosStatusAprovacao();
+        req.setAttribute("status", status);
+        req.getRequestDispatcher("view/CrudStatusAprovacao.jsp").forward(req, resp);
+    }
 }
