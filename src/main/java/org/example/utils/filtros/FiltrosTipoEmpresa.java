48e363fee73974ed0970ad8a1d290efbbd6be718
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2025-10-17 21:52:11 -0300

    feat: Adição final de métodos orquestradores de filtros

--- src/main/java/org/example/utils/filtros/FiltrosTipoEmpresa.java
+++ src/main/java/org/example/utils/filtros/FiltrosTipoEmpresa.java
@@ -1,57 +1,63 @@
 package org.example.utils.filtros;
 
+import org.example.model.StatusAprovacao;
 import org.example.model.TipoEmpresa;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FiltrosTipoEmpresa {
 
-    public List<TipoEmpresa> ordenarAtualizacoesRecentesTipoEmpresa(List<TipoEmpresa> tipoEmpresas){
+    public List<TipoEmpresa> ordenarAtualizacoesTipoEmpresa(List<TipoEmpresa> tipoEmpresas, boolean recente){
         for(int i = 0; i < tipoEmpresas.size(); i++){
             for(int j = 0; j < tipoEmpresas.size() + 1; i++){
-                if(tipoEmpresas.get(i).getUltimaAtualizacao().compareTo(tipoEmpresas.get(j).getUltimaAtualizacao()) > 0){
-                    TipoEmpresa apoio = tipoEmpresas.get(i);
-                    tipoEmpresas.set(i, tipoEmpresas.get(j));
-                    tipoEmpresas.set(j, apoio);
+                if(recente){
+                    if(tipoEmpresas.get(i).getUltimaAtualizacao().compareTo(tipoEmpresas.get(j).getUltimaAtualizacao()) > 0){
+                        TipoEmpresa apoio = tipoEmpresas.get(i);
+                        tipoEmpresas.set(i, tipoEmpresas.get(j));
+                        tipoEmpresas.set(j, apoio);
+                    }
+                }else{
+                    if(tipoEmpresas.get(i).getUltimaAtualizacao().compareTo(tipoEmpresas.get(j).getUltimaAtualizacao()) < 0){
+                        TipoEmpresa apoio = tipoEmpresas.get(i);
+                        tipoEmpresas.set(i, tipoEmpresas.get(j));
+                        tipoEmpresas.set(j, apoio);
+                    }
                 }
             }
         }
         return tipoEmpresas;
     }
 
-
-    public List<TipoEmpresa> ordenarAtualizacoesAntigasTipoEmpresa(List<TipoEmpresa> tipoEmpresas){
-        for(int i = 0; i < tipoEmpresas.size(); i++){
-            for(int j = 0; j < tipoEmpresas.size() + 1; i++){
-                if(tipoEmpresas.get(i).getUltimaAtualizacao().compareTo(tipoEmpresas.get(j).getUltimaAtualizacao()) < 0){
-                    TipoEmpresa apoio = tipoEmpresas.get(i);
-                    tipoEmpresas.set(i, tipoEmpresas.get(j));
-                    tipoEmpresas.set(j, apoio);
-                }
-            }
-        }
-        return tipoEmpresas;
-    }
-
-    public List<TipoEmpresa> ordenarSomenteTipoEmpresaAtivo(List<TipoEmpresa> tipoEmpresas){
+    public List<TipoEmpresa> ordenarTipoEmpresaPorStatus(List<TipoEmpresa> tipoEmpresas, char status){
         List<TipoEmpresa> tipoEmpresasAtivos = new LinkedList<>();
-        for(int i = 0; i < tipoEmpresas.size(); i++){
-            if(tipoEmpresas.get(i).getStatus() == 'a'){
-                tipoEmpresasAtivos.add(tipoEmpresas.get(i));
+        if(status == 'a' || status == 'i'){
+            for(int i = 0; i < tipoEmpresas.size(); i++){
+                if(tipoEmpresas.get(i).getStatus() == status){
+                    tipoEmpresasAtivos.add(tipoEmpresas.get(i));
+                }
             }
         }
         return tipoEmpresasAtivos;
     }
 
-    public List<TipoEmpresa> ordenarSomenteTipoEmpresaInativo(List<TipoEmpresa> tipoEmpresas){
-        List<TipoEmpresa> tipoEmpresasInativos = new LinkedList<>();
-        for(int i = 0; i < tipoEmpresas.size(); i++){
-            if(tipoEmpresas.get(i).getStatus() == 'i'){
-                tipoEmpresasInativos.add(tipoEmpresas.get(i));
-            }
+    public List<TipoEmpresa> ordenarTipoEmpresa(List<TipoEmpresa> tiposEmpresa , boolean ordenarStatus, Character status, boolean ordenarAtualizacoes, Boolean recente){
+        List<TipoEmpresa> tiposEmpresaOrdenados = tiposEmpresa;
+        if(!ordenarStatus && status != null ) {
+            throw new InvalidParameterException("Status recebido sem autorizalçao para ordenar Status");
         }
-        return tipoEmpresasInativos;
+        if(!ordenarAtualizacoes && recente != null){
+            throw new InvalidParameterException("Falta de autorização para ordenar atualizações");
+        }
+        if(ordenarStatus){
+            tiposEmpresaOrdenados = ordenarTipoEmpresaPorStatus(tiposEmpresa, status.charValue());
+        }
+        if(ordenarAtualizacoes){
+            tiposEmpresaOrdenados = ordenarAtualizacoesTipoEmpresa(tiposEmpresaOrdenados, recente);
+        }
+        return tiposEmpresaOrdenados;
     }
 
 
