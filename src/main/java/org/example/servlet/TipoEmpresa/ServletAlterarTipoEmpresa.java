4814b4c245c06020ca07cb3e9881960614ee0661
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2025-10-30 12:27:18 -0300

    style: Comentando codigo dos servlets de TipoEmpresa

--- src/main/java/org/example/servlet/TipoEmpresa/ServletAlterarTipoEmpresa.java
+++ src/main/java/org/example/servlet/TipoEmpresa/ServletAlterarTipoEmpresa.java
@@ -1,52 +1,110 @@
 package org.example.servlet.TipoEmpresa;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+///Classe criada com objetivo de realizar a alteração de registros da tabela TipoEmpresa através da interface JSP
 
+//Importações
+import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import org.example.conexao.ConexaoManager;
 import org.example.dao.TipoEmpresaDAO;
 import org.example.model.TipoEmpresa;
+import org.example.exceptions.FailedCommitException;
+import org.example.exceptions.FailedConnectionException;
+import org.example.exceptions.RollbackException;
 
+//ENDPOINT privado e com filtro do servlet (Área restrita)
 @WebServlet("/private/AlterarTipoEmpresa")
+
+//Abertura da classe de Servlet
 public class ServletAlterarTipoEmpresa extends HttpServlet {
+
+    //Metodo doPost com 2 ações diferentes, não manda informações pela URL
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
-        int action = Integer.parseInt(req.getParameter("action"));
-        int id = Integer.parseInt(req.getParameter("id"));
-        TipoEmpresaDAO tipoempresadao = new TipoEmpresaDAO();
+        try {
+            //Recebe qual ação será realizada
+            int action = Integer.parseInt(req.getParameter("action"));
 
-        TipoEmpresa tipoEmpresa = tipoempresadao.listarTipoEmpresaPorId(id);
+            //Recebe o id do registro que será modificado
+            int id = Integer.parseInt(req.getParameter("id"));
 
-        if (action == 0) {
-            // prepara atributos para a página de alteração
-            req.setAttribute("tipoEmpresa", tipoEmpresa);
-            req.setAttribute("popup-alterar", true);
-            req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
+            //Criando objeto com metodo responsáveis por ações na tabela TipoEmpresa do banco de dados
+            TipoEmpresaDAO tipoempresadao = new TipoEmpresaDAO();
 
-        } else if (action == 1) {
-            String nome = req.getParameter("nome");
-            String descricao = req.getParameter("descricao");
+            //Lista o tipoEmpresa pelo id do input
+            TipoEmpresa tipoEmpresa = tipoempresadao.listarTipoEmpresaPorId(id);
 
+            //Primeira ação (Abre o pop-up de alteração)
+            if (action == 0) {
+                //Seta o tipoEmpresa
+                req.setAttribute("tipoEmpresa", tipoEmpresa);
 
-            if (nome != "" && !tipoEmpresa.getNome().equals(nome)) {
-                tipoempresadao.alterarNomeTipoEmpresa(id, nome);
+                //Ativa o pop-up de alteração
+                req.setAttribute("popup-alterar", true);
+
+                //Carrega dados no CRUD com pop-up ativo
+                req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
+
+            } 
+            //Segunda ação (Altera tipoEmpresa do banco de dados)
+            else if (action == 1) {
+                //Recebe valores dos parâmetros do formulário
+                String nome = req.getParameter("nome");
+                String descricao = req.getParameter("descricao");
+
+                //Atualização do nome com validação
+                if (nome != "" && !tipoEmpresa.getNome().equals(nome)) {
+                    tipoempresadao.alterarNomeTipoEmpresa(id, nome);
+                }
+
+                //Atualização da descrição
+                if (!descricao.equals(tipoEmpresa.getDescricao())) {
+                    tipoempresadao.alterarDescricaoTipoEmpresa(id, descricao);
+                }
+
+                //Retorna mensagem de sucesso
+                req.setAttribute("erro", "Tipo empresa atualizado com sucesso!");
+                req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
             }
-            if (!descricao.equals(tipoEmpresa.getDescricao())) {
-                tipoempresadao.alterarDescricaoTipoEmpresa(id, descricao);
-            }
 
-            req.setAttribute("erro", "Tipo empresa atualizado com sucesso!");
-            req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
+        } 
+
+        //No caso de erros ao commitar ação no banco de dados
+        catch(FailedCommitException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro interno ao executar ação");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erros
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
         }
+
+        //No caso de erros ao conectar com o banco de dados
+        catch(FailedConnectionException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao conectar com banco de dados");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
+        //Em casos de erros ao desfazer ações no banco de dados
+        catch(RollbackException re){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao desfazer ação no banco de dados");
+            req.setAttribute("exception", re);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
     }
 
+    //Metodo com ações para ser executadas antes do fim do Servlet
     public void destroy() {
+
+        //Desconecta do banco de dados
         ConexaoManager.desconectar();
     }
 }
--- src/main/java/org/example/servlet/TipoEmpresa/ServletInserirTipoEmpresa.java
+++ src/main/java/org/example/servlet/TipoEmpresa/ServletInserirTipoEmpresa.java
@@ -1,43 +1,105 @@
 package org.example.servlet.TipoEmpresa;
 
-import java.io.IOException;
+///Classe criada com objetivo de inserir novos registros no banco de dados através da interface em JSP
 
+//Importações
+import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import org.example.conexao.ConexaoManager;
 import org.example.dao.TipoEmpresaDAO;
 import org.example.model.TipoEmpresa;
+import org.example.exceptions.FailedCommitException;
+import org.example.exceptions.FailedConnectionException;
+import org.example.exceptions.RollbackException;
 
+//ENDPOINT privado com filtro do Servlet
 @WebServlet("/private/InserirTipoEmpresa")
+
+//Abertura da classe de Servlet
 public class ServletInserirTipoEmpresa extends HttpServlet {
+
+    //Metodo doGet para carregar pop-up de inserir
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        //Ativa o pop-up de inserção
         req.setAttribute("popup-inserir", true);
+
+        //Envia dados para o CRUD
         req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
     }
 
+    //Metodo doPost para realizar inserção de tipoEmpresa no banco de dados
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String nome = req.getParameter("nome");
-        String descricao = req.getParameter("descricao");
-        TipoEmpresa tipoEmpresaNovo;
-        if (descricao == null || descricao.trim().isEmpty()) {
-            tipoEmpresaNovo = new TipoEmpresa(nome);
-        } else {
-            tipoEmpresaNovo = new TipoEmpresa(nome, descricao);
-        }
-        TipoEmpresaDAO tipoempresadao = new TipoEmpresaDAO();
-        if (tipoempresadao.inserirTipoEmpresa(tipoEmpresaNovo)) {
-            req.setAttribute("erro", "Tipo empresa inserido com sucesso!");
+        
+        try {
+            //Recebe parâmetros do formulário
+            String nome = req.getParameter("nome");
+            String descricao = req.getParameter("descricao");
+
+            //Objeto tipoEmpresa para inserção
+            TipoEmpresa tipoEmpresaNovo;
+
+            //Verifica se a descrição foi fornecida
+            if (descricao == null || descricao.trim().isEmpty()) {
+                //Cria objeto tipoEmpresa apenas com nome
+                tipoEmpresaNovo = new TipoEmpresa(nome);
+            } else {
+                //Cria objeto tipoEmpresa com nome e descrição
+                tipoEmpresaNovo = new TipoEmpresa(nome, descricao);
+            }
+
+            //Objeto de acesso a tabela no banco de dados
+            TipoEmpresaDAO tipoempresadao = new TipoEmpresaDAO();
+
+            //Insere e verifica se deu certo
+            if (tipoempresadao.inserirTipoEmpresa(tipoEmpresaNovo)) {
+                //Retorna que inserção deu certo
+                req.setAttribute("erro", "Tipo empresa inserido com sucesso!");
+            } else {
+                //Retorna mensagem de erro ao inserir tipoEmpresa
+                req.setAttribute("erro", "Não foi possível inserir tipo empresa");
+            }
+
+            //Envia dados para o CRUD
             req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
-        } else {
-            req.setAttribute("erro", "Não foi possível inserir tipo empresa");
-            req.getRequestDispatcher("/private/ListarTipoEmpresa").forward(req, resp);
+
+        } 
+
+        //No caso de erros ao commitar ação no banco de dados
+        catch(FailedCommitException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro interno ao executar ação");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erros
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
         }
+
+        //No caso de erros ao conectar com o banco de dados
+        catch(FailedConnectionException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao conectar com banco de dados");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
+        //Em casos de erros ao desfazer ações no banco de dados
+        catch(RollbackException re){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao desfazer ação no banco de dados");
+            req.setAttribute("exception", re);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
     }
 
+    //Metodo com ações para ser executadas no fim do servlet
     public void destroy() {
+        //Desconecta do banco de dados
         ConexaoManager.desconectar();
     }
 }
