ae92cd8cb7a65000e966791723392421d0458766
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2025-10-30 16:26:44 -0300

    style: Comentando javaScripts

--- src/main/webapp/js/fundo-azul.js
+++ src/main/webapp/js/fundo-azul.js
@@ -1,12 +1,21 @@
+///Esse javaScript altera o style do botão de "Entrar" no header, para quando o botão chegar a uma superfice roxa se tornar branco
+
+
+//Adiciona função na vizualização pelo scroll
 window.addEventListener('scroll', function () {
+
+    //Variável imutável com valor do primeiro elemento de classe "signup-button" (botão de login na plataforma)
     const signupButton = document.querySelector('.signup-button');
+    //Variável imutável com valor do primeiro elemento de ID "app-waves" (decoração de fundo)
     const secaoAzul = document.querySelector('#app-waves');
 
+    //Se não existir o elemento finaliza a função
     if (!secaoAzul) return;
 
+    //Armazena posição da seção azul em uma variável imutável
     const rect = secaoAzul.getBoundingClientRect();
 
-    // Quando o botão estiver sobre a seção azul
+    // Quando o botão estiver sobre a seção , adiciona novo modo de botão no css, para que o botão mude
     if (rect.top <= -75) {
         signupButton.classList.add('sobre-fundo-azul');
     } else {
