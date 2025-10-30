b5a82f8baf993f9e43d9f0f39f11a2d041c18ce1
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2025-10-30 12:26:39 -0300

    style: Comentando códigos dos servlets de MotivoFalta

--- src/main/java/org/example/servlet/MotivoFalta/ServletAlterarMotivoFalta.java
+++ src/main/java/org/example/servlet/MotivoFalta/ServletAlterarMotivoFalta.java
@@ -1,38 +1,113 @@
 package org.example.servlet.MotivoFalta;
 
-import java.io.IOException;
+///Classe criada com objetivo de realizar a alteração de registros da tabela MotivoFalta através da interface JSP
 
+//Importações
+import java.io.IOException;
+import java.util.List;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import org.example.conexao.ConexaoManager;
 import org.example.dao.MotivoFaltaDAO;
 import org.example.model.MotivoFalta;
+import org.example.exceptions.FailedCommitException;
+import org.example.exceptions.FailedConnectionException;
+import org.example.exceptions.RollbackException;
 
+//ENDPOINT privado e com filtro do servlet (Área restrita)
 @WebServlet("/private/AlterarMotivoFalta")
+
+//Abertura da classe de Servlet
 public class ServletAlterarMotivoFalta extends HttpServlet {
+
+    //Metodo doPost com 2 ações diferentes, não manda informações pela URL
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        int action = Integer.parseInt(req.getParameter("action"));
-        int id = Integer.parseInt(req.getParameter("id"));
-        MotivoFaltaDAO motivodao = new MotivoFaltaDAO();
-        if (action == 0) {
-            MotivoFalta motivo = motivodao.listarMotivoFaltaPorID(id);
-            req.setAttribute("motivo", motivo);
-            req.setAttribute("popup-alterar", true);
-            req.getRequestDispatcher("/private/ListarMotivosFalta").forward(req, resp);
-        } else if (action == 1) {
-            String motivo = req.getParameter("motivo");
 
+        try {
+            //Recebe qual ação será realizada
+            int action = Integer.parseInt(req.getParameter("action"));
 
-            if (motivodao.alterarMotivoMotivoFalta(id, motivo)) req.setAttribute("erro", "Adm atualizado com sucesso");
-            else req.setAttribute("erro", "Erro!");
-            java.util.List<MotivoFalta> motivos = motivodao.listarMotivosFalta();
-            req.setAttribute("motivos", motivos);
-            req.getRequestDispatcher("/WEB-INF/view/CrudMotivoFalta.jsp").forward(req, resp);
+            //Recebe o id do registro que será modificado
+            int id = Integer.parseInt(req.getParameter("id"));
+
+            //Criando objeto com metodo responsáveis por ações na tabela MotivoFalta do banco de dados
+            MotivoFaltaDAO motivodao = new MotivoFaltaDAO();
+
+            //Primeira ação (Abre o pop-up de alteração)
+            if (action == 0) {
+                //Objeto motivo com valores do registro retornado pelo metodo "listarMotivoFaltaPorID"
+                MotivoFalta motivo = motivodao.listarMotivoFaltaPorID(id);
+
+                //Seta o motivo
+                req.setAttribute("motivo", motivo);
+
+                //Ativa o pop-up de alteração
+                req.setAttribute("popup-alterar", true);
+
+                //Carrega dados no CRUD com pop-up ativo
+                req.getRequestDispatcher("/private/ListarMotivosFalta").forward(req, resp);
+
+            } 
+            //Segunda ação (Altera motivo falta do banco de dados)
+            else if (action == 1) {
+                //Recebe valor do motivo do parâmetro
+                String motivo = req.getParameter("motivo");
+
+                //Altera o motivo no banco de dados e verifica sucesso
+                if (motivodao.alterarMotivoMotivoFalta(id, motivo)) {
+                    //Retorna que o motivo foi alterado com sucesso
+                    req.setAttribute("erro", "Motivo atualizado com sucesso");
+                } else {
+                    //Retorna erro genérico
+                    req.setAttribute("erro", "Erro!");
+                }
+
+                //Lista todos os motivos
+                List<MotivoFalta> motivos = motivodao.listarMotivosFalta();
+
+                //Manda todos os motivos para serem listados no CRUD
+                req.setAttribute("motivos", motivos);
+
+                //Carrega dados no CRUD
+                req.getRequestDispatcher("/WEB-INF/view/CrudMotivoFalta.jsp").forward(req, resp);
+            }
+
+        } 
+
+        //No caso de erros ao commitar ação no banco de dados
+        catch(FailedCommitException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro interno ao executar ação");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erros
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
         }
+
+        //No caso de erros ao conectar com o banco de dados
+        catch(FailedConnectionException fce){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao conectar com banco de dados");
+            req.setAttribute("exception", fce);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
+        //Em casos de erros ao desfazer ações no banco de dados
+        catch(RollbackException re){
+            //Insere erro e exceção na página de erros
+            req.setAttribute("erro", "Erro ao desfazer ação no banco de dados");
+            req.setAttribute("exception", re);
+            //Envia dados para página de erro
+            req.getRequestDispatcher("/WEB-INF/view/ErrorPage.jsp").forward(req, resp);
+        }
+
     }
 
+    //Metodo com ações para ser executadas antes do fim do Servlet
     public void destroy() {
+
+        //Desconecta do banco de dados
         ConexaoManager.desconectar();
     }
 }
